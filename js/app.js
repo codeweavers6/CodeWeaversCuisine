e400fef9b9122e479685506a7349afad3cb9fdb9
yazan <yaz.sneneh@gmail.com>
2021-01-18 16:35:51 +0200

    create menu from javascript

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 ' use strict ';
 
 var arrayOfFood = [];
-var arrayOfPaths = ['food1.jpg','food2.jpg'];
-var arrayOfIngredients = ['Salt.Peper.Garlic','Onion.Bell peper.Parsley']; 
+var arrayOfPaths = ['food1.jpg', 'food2.jpg'];
+var arrayOfIngredients = ['Salt.Peper.Garlic', 'Onion.Bell peper.Parsley'];
 
 var firstItem = document.getElementById("first-item");
 var secondItem = document.getElementById("second-item");
@@ -11,7 +11,7 @@ var forthItem = document.getElementById("forth-item");
 var fifthItem = document.getElementById("fifth-item");
 var sixthItem = document.getElementById("sixth-item");
 
-function Meals (path,ingredients,price) {
+function Meals(name, path, ingredients, price) {
     this.path = 'img/' + path;
     this.name = path.split('.')[0];
     this.ingredients = ingredients;
@@ -19,14 +19,4 @@ function Meals (path,ingredients,price) {
     arrayOfFood.push(this);
 }
 
-for (var index = 0; index < arrayOfPaths.length; index++) {
-    new Meals(arrayOfPaths[index],arrayOfIngredients[index].split('.'));
-    // console.log(arrayOfFood[index].name);
-    // console.log(arrayOfFood[index].path);
-    // console.log(arrayOfFood[index].ingredients);
-}
-
-// console.log(arrayOfFood);
-
-firstItem.textContent = arrayOfFood[0].name;
 
